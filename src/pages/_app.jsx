ae7f041ce56ce05a3252507738eb19a8c388bc06
Veronique Lopez <veronique.lopez@ia.ca>
2023-07-26 08:57:06 -0400

    fix build?

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,6 @@
 // pages/_app.tsx
 
 import { ReactNode, useEffect, useState } from 'react';
-import type { NextComponentType } from 'next';
 import { AppContext, AppInitialProps, AppLayoutProps } from 'next/app';
 import { AuthenticationProvider } from '@/contexts/authentication/AuthenticationContext'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,12 +8,12 @@ import '@/styles/globals.css'
 import { AuthenticationState, noAuthentication } from '@/contexts/authentication/Authentication';
 import { resumeSession } from '@/contexts/authentication/AuthenticationClient';
 
-const MyApp: NextComponentType<AppContext, AppInitialProps, AppLayoutProps> = ({
+const MyApp = ({
   Component,
   pageProps,
-}: AppLayoutProps) => {
+}) => {
 
-  const getLayout = Component.getLayout || ((page: ReactNode) => page);
+  const getLayout = Component.getLayout || ((page) => page);
 
   return(
     <AuthenticationProvider>
