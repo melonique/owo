2c56bdb93d11e0f95056d9ce947bbca2d38fc73d
Alexandre Consigny <alexandre.consigny@gmail.com>
2023-08-25 12:28:30 -0400

    feat: allow test notifications

--- src/components/Layouts/PrivateLayout.tsx
+++ src/components/Layouts/PrivateLayout.tsx
@@ -1,9 +1,11 @@
 import React, { PropsWithChildren } from 'react';
 import { FooterNav } from '@/components'
 import usePrivatePage from '@/contexts/authentication/usePrivatePage';
+import { useNotificationBroadcaster } from '@/notifications/useNotificationBroadcaster';
 
 const PrivateLayout: React.FC<PropsWithChildren> = ({ children }) => {
   usePrivatePage()
+  useNotificationBroadcaster()
 
   return (
     <>
